48c272988d7424d62562d624a6d37eaf1d9b5872
Gorobet Nicolae <onyxpeek@gmail.com>
2023-01-15 17:13:00 +0200

    upd:update the js file

--- plants/index.js
+++ plants/index.js
@@ -1,6 +1,9 @@
+//hamburger menu for 380px;
 const toggleButton =document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', ()=>{
     navbarLinks.classList.toggle('active')
 })
+
+console.log('Score-90/100"\n"Вёрстка валидная +10 "\n"Вёрстка семантическая +20"\n"Вёрстка соответствует макету +40"\n"Требования к css + 12"\n"Интерактивность, реализуемая через css +20')
